b4b97222e77894b6e5df4a517fcbe616a164bfff
daenamcclintock <daena.mcclintock@gmail.com>
2022-04-27 14:55:05 -0400

    changes to message card

--- REEL_ESTATE/src/components/messages/ShowMessages.js
+++ REEL_ESTATE/src/components/messages/ShowMessages.js
@@ -4,9 +4,7 @@ import { removeMessage } from '../../api/messages'
 
 const ShowMessage = (props) => {
     // most of these are simply to pass to edit modal
-    const { message, user, property, triggerRefresh } = props
-
-
+    const { user, message, triggerRefresh } = props
 
     const destroyMessage = () => {
         removeMessage(user, user._id, message._id)
@@ -15,69 +13,37 @@ const ShowMessage = (props) => {
             .catch(console.error)
     }
 
-    // const displayMessage = () => {
-
-    // }
-
+    // function to map through the message details and display on a card
     const displayMessages = () => {
         let messages = user.messages
         return messages.map((message) => {
             console.log('THIS IS MESSAGE', message)
             return (
                 <Card className="m-2">
+                    <Card.Header>
+                        <h4><b>Name:</b> {message.fullName}</h4>
+                        <h5><b>Email:</b> {message.email}</h5>
+                        <h5><b>Phone Number:</b> {message.phoneNumber}</h5>
+                    </Card.Header>
                     <Card.Body>
                         <div>
-                            <h4>Name: {message.fullName}</h4>
-                            <h4>Email: {message.email}</h4>
-                            <h4>Phone Number: {message.phoneNumber}</h4>
-                            <h4>Message: {message.message}</h4>
+                            <h5>Message:</h5>
+                            <p>{message.message}</p>
                             <br></br>
                         </div>
-                        <Button onClick={()=> destroyMessage()} variant="outline-danger" size='sm'>
+                        <Button onClick={() => destroyMessage()} variant="outline-danger" size='sm'>
                             Delete Message    
                         </Button>
                     </Card.Body>
                 </Card>
             )
         })
-    }
-
-    let messages = user.messages
-    console.log('message', messages.map((message) => message.message))
-    console.log('fullName', messages.map((message) => message.fullName))
-    console.log('phoneNumber', messages.map((message) => message.phoneNumber))
-    console.log('Email', messages.map((message) => message.email))
-
-    console.log(displayMessages())
-
-    console.log('THIS IS THE USER', user)
-    console.log('THIS IS MESSAGES', user.messages)
-    console.log('THIS IS THE PROPERTY', property)
-    console.log('THIS IS THE MESSAGE', message)
-    
-
-// console.log('here is our message owner', message.owner) 
-// console.log('here is our message owner username', message.owner.username) 
+    } 
 
     return (
         <>
-            <Card className="m-2">
-                <Card.Body>
-                        {displayMessages()}
-                    {
-                        user && (user.id == true)
-                        ?
-                        <>
-                    <Button onClick={()=> destroyMessage()} variant="outline-danger" size='sm'>
-                        Delete Message    
-                    </Button>
+          {displayMessages()}
         </>
-        :
-        null
-        }
-            </Card.Body>
-    </Card>
-    </>
     )
 }
 
