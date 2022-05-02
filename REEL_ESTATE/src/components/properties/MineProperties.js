64cb101e8e69b657e9687a9894d0754d7fa7548d
daenamcclintock <daena.mcclintock@gmail.com>
2022-05-01 22:54:39 -0400

    added search functionality to Mine page

--- REEL_ESTATE/src/components/properties/MineProperties.js
+++ REEL_ESTATE/src/components/properties/MineProperties.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { Card, Dropdown, Button } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 import { getMyProperties } from '../../api/properties'
+import { BsSearch } from "react-icons/bs";
+import { FaBed } from "react-icons/fa";
+import { FaBath } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
 
 const cardContainerLayout = {
     display: 'flex',
@@ -13,6 +17,7 @@ const MineProperties = (props) => {
     const [myProperties, setMyProperties] = useState(null)
     const {user} = props
     const [property, setProperty] = useState(null)
+    const [searchTerm, setSearchTerm] = useState('')
     
 
     useEffect(() => {
@@ -37,40 +42,72 @@ const MineProperties = (props) => {
     let propertyCards
 
     if(myProperties.length > 0) {
-        propertyCards = myProperties.map(property => { 
-            return (<Card key={property._id} style={{ width: '25%' }} className="m-2">
-                <Card.Img 
-                src={property.image1} 
-                alt='property image' />
-                <Card.Body>
+        propertyCards = myProperties.filter((property) => {
+            if (searchTerm == '') {
+                return property
+            }
+            else if (property.address.split(',')[1].toLowerCase().includes(searchTerm.toLowerCase())) {
+                return property
+            }
+        }).map((property) => {
+            let cityState =  property.address.split(',')[1] + property.address.split(',')[2]
+            return (
+                <Card key={property._id} style={{ width: '30%' }} className="container m-2">
+                    <Card.Body>
                         <Link to={`/properties/${property._id}`}>
-                            <div className='imgIP'>
-                                <Card.ImgOverlay>
-                                    <Card.Title className='seller'>Seller: {property.owner.fullName}</Card.Title>
-                                </Card.ImgOverlay>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-20 col-sm-24">
+                                        <div class="box">
+                                            <img src={property.image1}/>
+                                            <div class="box-content">
+                                                <div class="overlay-img">
+                                                    <img src={property.image1}/>
+                                                </div>
+                                                <div class="inner-content">
+                                                    <h3 class="title">{property.address} </h3>
+                                                    <span class="post">Seller: {property.owner.fullName}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </Link>
-                </Card.Body>
-                <Card.Footer>
-                    <Card.Text>
-                        <Link to={`/properties/${property._id}`}>
-                            <Button variant="outline-dark">View Property</Button>
-                        </Link>
-                    </Card.Text>
-                    <Card.Text>
-                        {property.address}
-                    </Card.Text>
-                    <Card.Text>
-                        ${property.price / 1000000} M
-                    </Card.Text>
-                </Card.Footer>
-            </Card>)
-        })
-    }
+                    </Card.Body>
+                        <Card.Footer>
+                            <Card.Text>
+                                <div className='city-state'>
+                                    <h6><MdLocationOn /> {property.address.split(',')[1] + property.address.split(',')[2]}</h6>
+                                </div>
+                                <div className='property-metrics'>
+                                    <Link to={`/properties/${property._id}`}>
+                                        <Button variant="outline-dark">View Property</Button>
+                                    </Link>
+                                    <h6 className='property-price'>${property.price / 1000000} M &nbsp;&nbsp;&nbsp; <span><FaBed /> {property.bedrooms}</span> &nbsp;&nbsp;<span><FaBath /> {property.bedrooms}</span></h6>
+                                </div>
+                            </Card.Text>
+                        </Card.Footer>
+                </Card>
+
+            )
+    })
+}
 
     return (
         <>
             <h3 className='titleText'>My Property Listings</h3>
+            <div className='SearchBar_container'>
+                <div className="searchBar_fields">
+                    <div className="search_text">
+                        <h3>Search Luxury Real Estate</h3>
+                    </div>
+                    <div className="search_input">
+                        <input className='search_bar' type='text' placeholder='search by city' onChange={(e) => {setSearchTerm(e.target.value)}}/>
+                        <BsSearch />
+                    </div>
+                </div>
+            </div>
             <div style={cardContainerLayout}>
                 {propertyCards}
             </div>
